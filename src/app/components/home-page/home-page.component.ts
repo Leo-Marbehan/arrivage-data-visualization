17012a783630ccef20dfd249f589a77cd378dcf5
Leo-Marbehan <90802811+Leo-Marbehan@users.noreply.github.com>
2025-03-18 18:04:42 -0400

    Home page

---
+++ src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [MatButtonModule],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss',
+})
+export class HomePageComponent {
+  constructor(private readonly router: Router) {}
+
+  async navigateToRawData() {
+    await this.router.navigate(['/raw-data']);
+  }
+}
