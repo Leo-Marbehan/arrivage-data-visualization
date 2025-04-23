7f95e9c8e7245a5f16785ec792bde7afc8df4a3d
Léo Marbehan <marbehanleo@gmail.com>
2025-04-23 11:09:09 -0400

    Home page layout (#12)
    # Home page layout (#12)
     
    ## Modifications
    
    - Add Github link.
    - Add descriptions on home page.
    - Change 6 visualizations buttons for 3 sections.
    - Add toolbar in home page for consistency.
    
    ## Visuals
    
    ![image](https://github.com/user-attachments/assets/2a233196-75ec-4eb9-81d2-6fa38da90cbe)
    ![image](https://github.com/user-attachments/assets/b8fc62ff-e2f8-416d-a8a7-65de889e54f4)
    ![image](https://github.com/user-attachments/assets/7f0ef222-7696-45c2-8e61-88878727fbb2)

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { Navigation } from '../../classes/navigation.class';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule, ToolbarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
