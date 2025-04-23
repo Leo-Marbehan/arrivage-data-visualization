eb1f6c35bd41cad07bb71b3d7f4335962d046473
Leo-Marbehan <90802811+Leo-Marbehan@users.noreply.github.com>
2025-04-22 23:58:20 -0400

    Clean Home Page

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -3,10 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { Navigation } from '../../classes/navigation.class';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, ToolbarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
