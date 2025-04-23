34ee27c9b0e352b3327f0ef4b07d156bf041fd09
Leo-Marbehan <90802811+Leo-Marbehan@users.noreply.github.com>
2025-04-22 23:53:23 -0400

    Add github button

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { Navigation } from '../../classes/navigation.class';
 
 @Component({
   selector: 'app-home-page',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
