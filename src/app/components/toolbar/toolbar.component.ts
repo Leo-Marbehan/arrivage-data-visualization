cfbb59650807ee4a0ba8b112284c6baec99ecd8a
Leo-Marbehan <90802811+Leo-Marbehan@users.noreply.github.com>
2025-04-25 15:27:19 -0400

    Revert visualisation merges and clean up home page

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router } from '@angular/router';
 import { Navigation } from '../../classes/navigation.class';
@@ -8,7 +9,7 @@ import { Navigation } from '../../classes/navigation.class';
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatToolbarModule],
+  imports: [MatButtonModule, MatIconModule, MatToolbarModule, MatMenuModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
 })
