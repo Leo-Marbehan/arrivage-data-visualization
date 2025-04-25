8f3812794afe2bcecaef357caede52eb84152ca7
FatimaMel <89817688+FatimaMel@users.noreply.github.com>
2025-04-25 11:47:30 -0400

    Data viz1 (#15)
    * data viz 1
    
    * fixed css and html files
    
    * cleaned code
    
    * description viz 1
    
    * clean

---
+++ src/app/components/visualisation-1-page/visualisation-1-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Visualisation1PageComponent } from './visualisation-1-page.component';
+
+describe('Visualisation1PageComponent', () => {
+  let component: Visualisation1PageComponent;
+  let fixture: ComponentFixture<Visualisation1PageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Visualisation1PageComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Visualisation1PageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
